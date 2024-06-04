8f632e2d0cd77e1f8c03ef9aba187136b520ea37
JaxSpectre <unal.umut.98@gmail.com>
2024-06-04 10:51:37 +0300

    form

--- EFDiyetProgramiProje/Entities/Yemek.cs
+++ EFDiyetProgramiProje/Entities/Yemek.cs
@@ -12,5 +12,6 @@ namespace EFDiyetProgramiProje_DAL.Entities
         public string YemekAdi { get; set; }
         public string Birim { get; set; }
         public double Kalori { get; set; }
+        public int KategoriId { get; set; }
     }
 }
--- EFDiyetProgramiProje_PL/KullaniciKontrolPaneli.Designer.cs
+++ EFDiyetProgramiProje_PL/KullaniciKontrolPaneli.Designer.cs
@@ -44,19 +44,43 @@
             label3 = new Label();
             label4 = new Label();
             button2 = new Button();
-            listBox1 = new ListBox();
+            yemekler = new ListBox();
             button3 = new Button();
+            comboBox1 = new ComboBox();
+            label13 = new Label();
+            label14 = new Label();
+            comboBox2 = new ComboBox();
+            panel1 = new Panel();
+            label15 = new Label();
+            label16 = new Label();
+            button4 = new Button();
+            panel2 = new Panel();
+            button5 = new Button();
+            label17 = new Label();
+            label18 = new Label();
+            comboBox3 = new ComboBox();
+            panel3 = new Panel();
+            button6 = new Button();
+            label19 = new Label();
+            label20 = new Label();
+            comboBox4 = new ComboBox();
+            label21 = new Label();
+            label22 = new Label();
+            groupBox1 = new GroupBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
             SuspendLayout();
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(7, 18);
+            monthCalendar1.Location = new Point(9, 37);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(28, 393);
+            button1.Location = new Point(30, 412);
             button1.Name = "button1";
             button1.Size = new Size(133, 45);
             button1.TabIndex = 2;
@@ -66,7 +90,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(107, 346);
+            label9.Location = new Point(109, 365);
             label9.Name = "label9";
             label9.Size = new Size(52, 15);
             label9.TabIndex = 39;
@@ -75,7 +99,7 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(107, 299);
+            label10.Location = new Point(109, 318);
             label10.Name = "label10";
             label10.Size = new Size(27, 15);
             label10.TabIndex = 38;
@@ -84,7 +108,7 @@
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(107, 252);
+            label11.Location = new Point(109, 271);
             label11.Name = "label11";
             label11.Size = new Size(30, 15);
             label11.TabIndex = 37;
@@ -93,7 +117,7 @@
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(107, 205);
+            label12.Location = new Point(109, 224);
             label12.Name = "label12";
             label12.Size = new Size(95, 15);
             label12.TabIndex = 36;
@@ -102,7 +126,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(23, 346);
+            label5.Location = new Point(25, 365);
             label5.Name = "label5";
             label5.Size = new Size(52, 15);
             label5.TabIndex = 35;
@@ -111,7 +135,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(48, 299);
+            label6.Location = new Point(50, 318);
             label6.Name = "label6";
             label6.Size = new Size(27, 15);
             label6.TabIndex = 34;
@@ -120,7 +144,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(45, 252);
+            label7.Location = new Point(47, 271);
             label7.Name = "label7";
             label7.Size = new Size(30, 15);
             label7.TabIndex = 33;
@@ -129,7 +153,7 @@
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(45, 205);
+            label8.Location = new Point(47, 224);
             label8.Name = "label8";
             label8.Size = new Size(30, 15);
             label8.TabIndex = 32;
@@ -156,7 +180,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(729, 103);
+            label3.Location = new Point(696, 103);
             label3.Name = "label3";
             label3.Size = new Size(35, 15);
             label3.TabIndex = 42;
@@ -165,7 +189,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(1004, 103);
+            label4.Location = new Point(962, 98);
             label4.Name = "label4";
             label4.Size = new Size(46, 15);
             label4.TabIndex = 43;
@@ -173,38 +197,238 @@
             // 
             // button2
             // 
-            button2.Location = new Point(693, 444);
+            button2.Location = new Point(759, 545);
             button2.Name = "button2";
             button2.Size = new Size(116, 43);
             button2.TabIndex = 44;
             button2.Text = "Güncelle";
             button2.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // yemekler
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(271, 132);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(112, 364);
-            listBox1.TabIndex = 45;
+            yemekler.FormattingEnabled = true;
+            yemekler.ItemHeight = 15;
+            yemekler.Location = new Point(271, 132);
+            yemekler.Name = "yemekler";
+            yemekler.Size = new Size(134, 364);
+            yemekler.TabIndex = 45;
             // 
             // button3
             // 
-            button3.Location = new Point(28, 474);
+            button3.Location = new Point(30, 493);
             button3.Name = "button3";
             button3.Size = new Size(133, 45);
             button3.TabIndex = 46;
             button3.Text = "Kullanıcı Bilgilerimi Değiştir";
             button3.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(271, 95);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(134, 23);
+            comboBox1.TabIndex = 47;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(271, 66);
+            label13.Name = "label13";
+            label13.Size = new Size(100, 15);
+            label13.TabIndex = 48;
+            label13.Text = "Yemek Kategorisi:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(3, 18);
+            label14.Name = "label14";
+            label14.Size = new Size(61, 15);
+            label14.TabIndex = 49;
+            label14.Text = "yemek adı";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(135, 15);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(66, 23);
+            comboBox2.TabIndex = 50;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(label15);
+            panel1.Controls.Add(label14);
+            panel1.Controls.Add(comboBox2);
+            panel1.Location = new Point(447, 132);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(231, 88);
+            panel1.TabIndex = 51;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(83, 18);
+            label15.Name = "label15";
+            label15.Size = new Size(30, 15);
+            label15.TabIndex = 51;
+            label15.Text = "adet";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(450, 458);
+            label16.Name = "label16";
+            label16.Size = new Size(82, 15);
+            label16.TabIndex = 52;
+            label16.Text = "Toplam Kalori:";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(135, 44);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 52;
+            button4.Text = "Sil";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(label17);
+            panel2.Controls.Add(label18);
+            panel2.Controls.Add(comboBox3);
+            panel2.Location = new Point(693, 132);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(231, 88);
+            panel2.TabIndex = 53;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(135, 44);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 52;
+            button5.Text = "Sil";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(83, 18);
+            label17.Name = "label17";
+            label17.Size = new Size(30, 15);
+            label17.TabIndex = 51;
+            label17.Text = "adet";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(3, 18);
+            label18.Name = "label18";
+            label18.Size = new Size(61, 15);
+            label18.TabIndex = 49;
+            label18.Text = "yemek adı";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(135, 15);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(66, 23);
+            comboBox3.TabIndex = 50;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button6);
+            panel3.Controls.Add(label19);
+            panel3.Controls.Add(label20);
+            panel3.Controls.Add(comboBox4);
+            panel3.Location = new Point(942, 132);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(231, 88);
+            panel3.TabIndex = 53;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(135, 44);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 52;
+            button6.Text = "Sil";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(83, 18);
+            label19.Name = "label19";
+            label19.Size = new Size(30, 15);
+            label19.TabIndex = 51;
+            label19.Text = "adet";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(3, 18);
+            label20.Name = "label20";
+            label20.Size = new Size(61, 15);
+            label20.TabIndex = 49;
+            label20.Text = "yemek adı";
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(135, 15);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(66, 23);
+            comboBox4.TabIndex = 50;
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(675, 458);
+            label21.Name = "label21";
+            label21.Size = new Size(82, 15);
+            label21.TabIndex = 54;
+            label21.Text = "Toplam Kalori:";
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Location = new Point(945, 458);
+            label22.Name = "label22";
+            label22.Size = new Size(82, 15);
+            label22.TabIndex = 55;
+            label22.Text = "Toplam Kalori:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(1250, 82);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(192, 436);
+            groupBox1.TabIndex = 56;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Günlük/Haftalık Rapor";
+            // 
             // KullaniciKontrolPaneli
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1075, 531);
+            ClientSize = new Size(1501, 600);
+            Controls.Add(groupBox1);
+            Controls.Add(label22);
+            Controls.Add(label21);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(label16);
+            Controls.Add(panel1);
+            Controls.Add(label13);
+            Controls.Add(comboBox1);
             Controls.Add(button3);
-            Controls.Add(listBox1);
+            Controls.Add(yemekler);
             Controls.Add(button2);
             Controls.Add(label4);
             Controls.Add(label3);
@@ -222,6 +446,12 @@
             Controls.Add(monthCalendar1);
             Name = "KullaniciKontrolPaneli";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -244,7 +474,28 @@
         private Label label3;
         private Label label4;
         private Button button2;
-        private ListBox listBox1;
+        private ListBox yemekler;
         private Button button3;
+        private ComboBox comboBox1;
+        private Label label13;
+        private Label label14;
+        private ComboBox comboBox2;
+        private Panel panel1;
+        private Label label15;
+        private Label label16;
+        private Button button4;
+        private Panel panel2;
+        private Button button5;
+        private Label label17;
+        private Label label18;
+        private ComboBox comboBox3;
+        private Panel panel3;
+        private Button button6;
+        private Label label19;
+        private Label label20;
+        private ComboBox comboBox4;
+        private Label label21;
+        private Label label22;
+        private GroupBox groupBox1;
     }
 }
